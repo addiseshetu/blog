abd2315565db5235025fbca4799d198137e43cc1
Addis Eshetu <addiseshetu@Addiss-MacBook-Pro.local>
2023-02-10 08:52:01 -0500

    added index page

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def post_list(request):
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'post_list.html', context)
